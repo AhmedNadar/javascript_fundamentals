96ed72fcc7ba51a96386a69fe4c2136ea121f969
Ahmed Nadar <ahmedfay@hotmail.com>
2013-09-24 16:14:11 -0400

    Finished Arrays

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -4,7 +4,10 @@
 
 // Create an array to hold your top choices (colors, presidents, whatever).
 // For each choice, log to the screen a string like: "My #1 choice is blue."
-// Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
+// Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", 
+// picking the right suffix for the number based on what it is.
 
 
+var cars = ["BMW", "VW", "Audi"];
 
+console.log("My 1st Car would be " + cars[0] + " and my 2ed choice would beb " +cars[1]+ "and if i have to, I'm ok with my 3rd choice " + cars[2]+".");
